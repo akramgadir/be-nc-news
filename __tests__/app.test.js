375f5933056ca6d25b59fb4349d86d23286c0853
akramgadir <akram.gadir@gmail.com>
2023-10-03 15:42:32 +0100

    q4 done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,3 +47,41 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("returns 200 status code", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles.article_id).toBe(2);
+        expect(res.body.articles.title).toEqual("Sony Vaio; or, The Laptop");
+        expect(res.body.articles.author).toEqual("icellusedkars");
+        expect(res.body.articles.votes).toEqual(0);
+        expect(res.body.articles.article_img_url).toEqual(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+        expect(res.body.articles).toHaveProperty("created_at");
+        expect(res.body.articles).toHaveProperty("body");
+        expect(res.body.articles).toHaveProperty("topic");
+      });
+  });
+  test("returns 404 error status code", () => {
+    return request(app)
+      .get("/api/articles/2000")
+      .expect(404)
+      .then(({ body }) => {
+        //could also not {} and just do res.body and use res.body under instead too
+        expect(body.message).toBe("not found");
+      });
+  });
+  test("returns 400 error status code", () => {
+    return request(app)
+      .get("/api/articles/banana")
+      .expect(400)
+      .then(({ body }) => {
+        //could also not {} and just do res.body and use res.body under instead too
+        expect(body.message).toBe("PSQL error");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const {
   getArticleById,
 } = require("./controllers/topics.controllers.js");
 const endpoints = require("./endpoints.json");
-//ONCE PR IS APPROVED, MERGE ON GITHUB CHECKECOUT TO MAIN BRANCH AND THEN PULL
-//MAKE THE NEW BRANCH FIRST THING AKRAM 4
+const {
+  handle500Errors,
+  handleCustomErrors,
+  handlePSQLErrors,
+} = require("./error-handling");
 const app = express();
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send("Server Error!");
-});
 
 // check api is running ok
 // app.get("/api/topics", (req, res) => {
@@ -25,5 +23,10 @@ app.get("/api", (req, res, next) => {
   res.json(endpoints);
 });
 
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
+app.use(handle500Errors); //comes last as its the backup error, the order matters here not in the handling file
+
 module.exports = app;
-// module.exports = { app };
